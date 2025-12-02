e159b11224b4d8a2e93cae7f19dc23be4ec8e297
PedroPeyroteo <pedropeyroteo@outlook.com>
2025-12-02 13:14:36 +0000

    feat(app): wire suggestions into controllers and menus

--- src/app/menu.c
+++ src/app/menu.c
@@ -5,6 +5,7 @@
 #include "app/user_controller.h"
 #include "app/book_controller.h"
 #include "app/loan_controller.h"
+#include "app/suggestion_controller.h"
 
 /*
 	Menus simples que mostram o fluxo:
@@ -16,63 +17,61 @@
 void menuClientes(DB *db)
 {
 	printf("\n=== MENU CLIENTES ===\n");
-	void menuLivros(DB *db)
+	user_list_all(db);
+}
+
+void menuLivros(DB *db)
+{
+	int opcao;
+
+	do
 	{
-		int opcao;
+		printf("\n=== MENU LIVROS ===\n");
+		printf("1. Inserir novo livro\n");
+		printf("2. Editar livro\n");
+		printf("3. Eliminar livro\n");
+		printf("4. Pesquisar livros\n");
+		printf("5. Duplicar livro\n");
+		printf("6. Listar todos os livros\n");
+		printf("0. Voltar ao menu principal\n");
+		printf("Escolha uma opcao: ");
 
-		do
+		if (scanf("%d", &opcao) != 1)
 		{
-			printf("\n=== MENU LIVROS ===\n");
-			printf("1. Inserir novo livro\n");
-			printf("2. Editar livro\n");
-			printf("3. Eliminar livro\n");
-			printf("4. Pesquisar livros\n");
-			printf("5. Duplicar livro\n");
-			printf("6. Listar todos os livros\n");
-			printf("0. Voltar ao menu principal\n");
-			printf("Escolha uma opcao: ");
+			printf("Opcao invalida.\n");
+			opcao = -1;
+		}
+		while (getchar() != '\n')
+			;
 
-			if (scanf("%d", &opcao) != 1)
-			{
-				printf("Opcao invalida.\n");
-				opcao = -1;
-			}
-			while (getchar() != '\n')
-				;
-
-			switch (opcao)
-			{
-			case 1:
-				book_insert(db);
-				break;
-			case 2:
-				book_edit(db);
-				break;
-			case 3:
-				book_delete(db);
-				break;
-			case 4:
-				book_search(db);
-				break;
-			case 5:
-				book_duplicate(db);
-				break;
-			case 6:
-				book_list_all(db);
-				break;
-			case 0:
-				printf("A sair do menu de livros.\n");
-				break;
-			default:
-				printf("Opcao invalida. Tente novamente.\n");
-				break;
-			}
-		} while (opcao != 0);
-	}
+		switch (opcao)
+		{
+		case 1:
+			book_insert(db);
+			break;
+		case 2:
+			book_edit(db);
+			break;
+		case 3:
+			book_delete(db);
+			break;
+		case 4:
+			book_search(db);
+			break;
+		case 5:
+			book_duplicate(db);
+			break;
+		case 6:
+			book_list_all(db);
+			break;
+		case 0:
+			printf("A sair do menu de livros.\n");
+			break;
+		default:
+			printf("Opcao invalida. Tente novamente.\n");
 			break;
 		}
 	} while (opcao != 0);
->>>>>>> nadia-dev
 }
 
 void menuEmprestimos(DB *db)
@@ -83,7 +82,38 @@ void menuEmprestimos(DB *db)
 
 void menuSugestoes(DB *db)
 {
-	(void)db;
-	printf("\n=== MENU SUGESTOES ===\n");
-	printf("[sugestoes] Funcionalidade em desenvolvimento.\n");
+	int opcao;
+
+	do
+	{
+		printf("\n=== MENU SUGESTOES ===\n");
+		printf("1. Registar sugestao\n");
+		printf("2. Listar sugestoes\n");
+		printf("0. Voltar ao menu principal\n");
+		printf("Escolha uma opcao: ");
+
+		if (scanf("%d", &opcao) != 1)
+		{
+			printf("Opcao invalida.\n");
+			opcao = -1;
+		}
+		while (getchar() != '\n')
+			;
+
+		switch (opcao)
+		{
+		case 1:
+			suggestion_register(db);
+			break;
+		case 2:
+			suggestion_list_all(db);
+			break;
+		case 0:
+			printf("A sair do menu de sugestoes.\n");
+			break;
+		default:
+			printf("Opcao invalida. Tente novamente.\n");
+			break;
+		}
+	} while (opcao != 0);
 }
